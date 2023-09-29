b5d12a0199b14d56864174648acad9ee0bca9fd2
hamitLicina <hamitlicina@gmail.com>
2023-09-29 12:27:16 +0300

    Pages boilerplate added

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <div>
-       
+       <h1>Hello React</h1>
       </div>
     </>
   )
---
+++ src/pages/CategoryArticle/CategoryArticle.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './CategoryArticle.css'
+
+
+function CategoryArticle() {
+  return (
+    <div>CategoryArticle</div>
+  )
+}
+
+export default CategoryArticle
