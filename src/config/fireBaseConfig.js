3436135e41b828d1b9799cd314135d1135ea5aa9
hamitLicina <hamitlicina@gmail.com>
2023-09-30 10:11:03 +0300

    First lesson firebaseConfig part added, rest of 40.th minute finished

--- src/config/fireBaseConfig.js
+++ src/config/fireBaseConfig.js
@@ -4,6 +4,8 @@ import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+import { getFirestore } from "firebase/firestore";
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -19,3 +21,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+// Set up database and export it
+export const db = getFirestore(app)
